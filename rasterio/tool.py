ea9a397ca7a67300ceeb609fe378fbc0b7a80273
Sean Gillies <sean@mapbox.com>
2014-01-31 09:50:29 -0700

    Improvements to the interpreter/tool.

--- rasterio/tool.py
+++ rasterio/tool.py
@@ -1,19 +1,25 @@
 
 import code
+import logging
 import pprint
+import sys
 
 import rasterio
 
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+logger = logging.getLogger('rasterio')
+
+
 def main(srcfile):
     
-    with rasterio.drivers():
-
-        with rasterio.open(srcfile) as src:
+    with rasterio.drivers(), rasterio.open(srcfile) as src:
             
-            code.interact(
-                'Rasterio Interactive Interpreter\n'
-                'Type "src.name", "src.read_band(1)", or "help(src)" for more information',
-                local=locals())
+        code.interact(
+            'Rasterio %s Interactive Interpreter\n'
+            'Type "src.name", "src.read_band(1)", or "help(src)" '
+            'for more information.' %  rasterio.__version__,
+            local=locals())
 
 
 if __name__ == '__main__':
@@ -21,11 +27,12 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(
+        prog="python -m rasterio.tool",
         description="Open a dataset and drop into an interactive interpreter")
     parser.add_argument(
         'src', 
-        metavar='INPUT', 
-        help="Input file names")
+        metavar='FILE', 
+        help="Input dataset file name")
     args = parser.parse_args()
     
     main(args.src)
