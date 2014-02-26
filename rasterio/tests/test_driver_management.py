a626e68b6a6b2d1ad2fd8f2c0f95e5f34ebadaab
Sean Gillies <sean@mapbox.com>
2014-02-26 14:52:54 -0700

    Test of options setting.

--- rasterio/tests/test_driver_management.py
+++ rasterio/tests/test_driver_management.py
@@ -15,3 +15,17 @@ def test_drivers():
         assert driver_count() > 0
         assert type(n) == GDALEnv
 
+def test_options(tmpdir):
+    """Test that setting CPL_DEBUG=True results in GDAL debug messages.
+    """
+    logger = logging.getLogger('rasterio')
+    logger.setLevel(logging.DEBUG)
+    logfile = str(tmpdir.join('test_options.log'))
+    fh = logging.FileHandler(logfile)
+    logger.addHandler(fh)
+    with rasterio.drivers(CPL_DEBUG=True):
+        with rasterio.open("rasterio/tests/data/RGB.byte.tif") as src:
+            pass
+        log = open(logfile).read()
+        assert "GDAL: GDALOpen(rasterio/tests/data/RGB.byte.tif" in log
+
