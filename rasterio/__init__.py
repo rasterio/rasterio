b7d9c611dabe85566599d2c5f14f8cdd7d5305a6
Sean Gillies <sean.gillies@gmail.com>
2014-03-22 15:10:34 -0600

    Bump version to 0.7.3.

--- rasterio/__init__.py
+++ rasterio/__init__.py
@@ -17,7 +17,7 @@ from rasterio.dtypes import (
 
 
 __all__ = ['open', 'drivers', 'copy', 'check_dtype']
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 log = logging.getLogger('rasterio')
 class NullHandler(logging.Handler):
