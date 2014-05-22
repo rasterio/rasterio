ce6603c0fec96370cf980c48efe087cdb48953a8
Sean Gillies <sean@mapbox.com>
2014-05-22 13:05:16 -0600

    Make xoff and yoff aliases for c and f.
    Also derived AffineMatrix from an anonymous namedtuple!

--- rasterio/__init__.py
+++ rasterio/__init__.py
@@ -139,14 +139,34 @@ def drivers(**kwargs):
         return GDALEnv(False, **kwargs)
 
 
-AffineMatrixBase = namedtuple(
-                    'AffineMatrix',  ('a', 'b', 'xoff', 'd', 'e', 'yoff'))
+class AffineMatrix(
+        namedtuple('AffineMatrix',  ('a', 'b', 'c', 'd', 'e', 'f'))):
+    """
+    The augmented affine transformation matrix.
 
-class AffineMatrix(AffineMatrixBase):
+      | x' |   | a  b  c | | x |
+      | y' | = | d  e  f | | y |
+      | 1  |   | 0  0  1 | | 1 |
+
+    The vector on the left hand side is position in world coordinates
+    and the vector on the right hand side, image/array coordinates.
+
+    Note that c and f are the world coordinates at the image/array
+    origin (upper left corner).
+    """
 
     @classmethod
-    def from_gdal(self, xoff, a, b, yoff, d, e):
-        return AffineMatrix(a, b, xoff, d, e, yoff)
+    def from_gdal(self, c, a, b, f, d, e):
+        return AffineMatrix(a, b, c, d, e, f)
 
     def to_gdal(self):
-        return (self.xoff, self.a, self.b, self.yoff, self.d, self.e)
+        return (self.c, self.a, self.b, self.f, self.d, self.e)
+
+    @property
+    def xoff(self):
+        return self.c
+
+    @property
+    def yoff(self):
+        return self.f
+
--- rasterio/tests/test_transform.py
+++ rasterio/tests/test_transform.py
@@ -3,10 +3,10 @@ from rasterio import AffineMatrix
 
 def test_gdal():
     t = AffineMatrix.from_gdal(-237481.5, 425.0, 0.0, 237536.4, 0.0, -425.0)
-    assert t.xoff == -237481.5
+    assert t.c == t.xoff == -237481.5
     assert t.a == 425.0
     assert t.b == 0.0
-    assert t.yoff ==  237536.4
+    assert t.f == t.yoff ==  237536.4
     assert t.d == 0.0
     assert t.e == -425.0
     assert tuple(t) == (425.0, 0.0, -237481.5, 0.0, -425.0, 237536.4)
