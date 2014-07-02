716e7d65cf3ea8db3d710905f3e22d898a8fd411
Amit Kapadia <akapad@gmail.com>
2014-07-01 18:17:19 -0700

    add enums for colorinterp

--- rasterio/tests/test_colorinterp.py
+++ rasterio/tests/test_colorinterp.py
@@ -1,4 +1,7 @@
+
 import rasterio
+from rasterio.enums import ColorInterp
+
 
 def test_colorinterp(tmpdir):
     
@@ -8,9 +11,9 @@ def test_colorinterp(tmpdir):
             r, g, b = map(src.read_band, (1, 2, 3))
             meta = src.meta
             
-            assert src.colorinterp(1) == 3
-            assert src.colorinterp(2) == 4
-            assert src.colorinterp(3) == 5
+            assert src.colorinterp(1) == ColorInterp.red
+            assert src.colorinterp(2) == ColorInterp.green
+            assert src.colorinterp(3) == ColorInterp.blue
             
         tiffname = str(tmpdir.join('foo.tif'))
         
@@ -19,10 +22,10 @@ def test_colorinterp(tmpdir):
             dst.write_band(2, g)
             dst.write_band(3, b)
             
-            dst.write_colorinterp(1, 1)
-            dst.write_colorinterp(2, 1)
-            dst.write_colorinterp(3, 1)
+            dst.write_colorinterp(1, ColorInterp.gray)
+            dst.write_colorinterp(2, ColorInterp.grey)
+            dst.write_colorinterp(3, ColorInterp.gray)
             
-            assert dst.colorinterp(1) == 1
-            assert dst.colorinterp(2) == 1
-            assert dst.colorinterp(3) == 1
+            assert dst.colorinterp(1) == ColorInterp.gray
+            assert dst.colorinterp(2) == ColorInterp.grey
+            assert dst.colorinterp(3) == ColorInterp.gray
