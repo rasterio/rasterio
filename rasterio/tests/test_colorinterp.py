f733459c539c6712321e58259e82bc18d8487b28
Sean Gillies <sean@mapbox.com>
2014-07-02 14:47:46 -0600

    Test of CMYK photometric option.

--- rasterio/tests/test_colorinterp.py
+++ rasterio/tests/test_colorinterp.py
@@ -8,10 +8,18 @@ def test_colorinterp(tmpdir):
     with rasterio.drivers():
 
         with rasterio.open('rasterio/tests/data/RGB.byte.tif') as src:
-            r, g, b = map(src.read_band, (1, 2, 3))
-            meta = src.meta
-            
             assert src.colorinterp(1) == ColorInterp.red
             assert src.colorinterp(2) == ColorInterp.green
             assert src.colorinterp(3) == ColorInterp.blue
+            
+        tiffname = str(tmpdir.join('foo.tif'))
+        
+        meta = src.meta
+        meta['photometric'] = 'CMYK'
+        meta['count'] = 4
+        with rasterio.open(tiffname, 'w', **meta) as dst:
+            assert dst.colorinterp(1) == ColorInterp.cyan
+            assert dst.colorinterp(2) == ColorInterp.magenta
+            assert dst.colorinterp(3) == ColorInterp.yellow
+            assert dst.colorinterp(4) == ColorInterp.black
 
