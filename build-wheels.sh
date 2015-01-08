82fe909259c70eb8e4a128ee92bdaa84276800bd
Sean Gillies <sean@mapbox.com>
2015-01-07 21:40:53 -0700

    Add 10_10 tags.

--- build-wheels.sh
+++ build-wheels.sh
@@ -3,7 +3,7 @@
 # Automation of this is a TODO. For now, it depends on manually built libraries
 # as detailed in https://gist.github.com/sgillies/a8a2fb910a98a8566d0a.
 
-export MACOSX_DEPLOYMENT_TARGET=10.7
+export MACOSX_DEPLOYMENT_TARGET=10.6
 export GDAL_CONFIG="/usr/local/bin/gdal-config"
 export PACKAGE_DATA=1
 
@@ -14,5 +14,5 @@ CFLAGS="`$GDAL_CONFIG --cflags`" LDFLAGS="`$GDAL_CONFIG --libs` `$GDAL_CONFIG --
 source $HOME/envs/riowhl34/bin/activate
 CFLAGS="`$GDAL_CONFIG --cflags`" LDFLAGS="`$GDAL_CONFIG --libs` `$GDAL_CONFIG --dep-libs`" python setup.py bdist_wheel -d wheels/$VERSION
 
-find wheels/$VERSION -name rasterio-$VERSION*.whl -exec delocate-wheel -w fixed_wheels/$VERSION --require-archs=intel -v {} \;
-find fixed_wheels/$VERSION -name *.whl -exec rename s/macosx_10_6_intel/macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64/ {} \;
+parallel delocate-wheel -w fixed_wheels/$VERSION --require-archs=intel -v {} ::: wheels/$VERSION/*.whl
+parallel cp {} {.}.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl ::: fixed_wheels/$VERSION/*.whl
