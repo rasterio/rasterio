aff47450ccdd6027475921d0583c7478bf205e48
Sean Gillies <sean@mapbox.com>
2015-01-20 16:20:42 -0700

    Note fix of #254, bump patch level.

--- rasterio/__init__.py
+++ rasterio/__init__.py
@@ -18,7 +18,7 @@ from rasterio.transform import Affine, guard_transform
 
 __all__ = [
     'band', 'open', 'drivers', 'copy', 'pad']
-__version__ = "0.17"
+__version__ = "0.17.1"
 
 log = logging.getLogger('rasterio')
 class NullHandler(logging.Handler):
