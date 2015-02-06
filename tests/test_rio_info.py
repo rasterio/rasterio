c3732f7c76b88860310aa1ff23e05072be83ef63
Sean Gillies <sean@mapbox.com>
2015-02-06 14:29:14 -0700

    Add new fields to rio-info.
    Closes #269.
    
    Required moving _transform() from _warp to _base so we could
    avoid import Numpy as long as possible.
    
    Also added stats to info output in --verbose mode, also accessible
    using the --stats option.

--- tests/test_rio_info.py
+++ tests/test_rio_info.py
@@ -77,6 +77,37 @@ def test_info_res():
     runner = CliRunner()
     result = runner.invoke(
         info.info,
-        ['tests/data/RGB.byte.tif', '--res])
+        ['tests/data/RGB.byte.tif', '--res'])
     assert result.exit_code == 0
-    assert result.output == '300.0 300.0\n'
+    assert result.output.startswith('300.037')
+
+
+def test_info_lnglat():
+    runner = CliRunner()
+    result = runner.invoke(
+        info.info,
+        ['tests/data/RGB.byte.tif', '--lnglat'])
+    assert result.exit_code == 0
+    assert result.output.startswith('-77.757')
+
+
+def test_mo_info():
+    runner = CliRunner()
+    result = runner.invoke(info.info, ['tests/data/RGB.byte.tif'])
+    assert result.exit_code == 0
+    assert '"res": [300.037' in result.output
+    assert '"lnglat": [-77.757' in result.output
+
+
+def test_info_stats():
+    runner = CliRunner()
+    result = runner.invoke(info.info, ['tests/data/RGB.byte.tif', '--tell-me-more'])
+    assert result.exit_code == 0
+    assert '"stats": [{"max": 255.0, "mean": 44.4344' in result.output
+
+
+def test_info_stats_only():
+    runner = CliRunner()
+    result = runner.invoke(info.info, ['tests/data/RGB.byte.tif', '--stats', '--bidx', '2'])
+    assert result.exit_code == 0
+    assert result.output.startswith('1.000000 255.000000 66.02')
