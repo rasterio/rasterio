d8f3d9da30380ebb42907eba39d806870054b64e
Sean Gillies <sean.gillies@gmail.com>
2015-02-08 09:34:23 -0700

    Syntax for multipart/band commands.

--- rasterio/rio/calc.py
+++ rasterio/rio/calc.py
@@ -17,8 +17,19 @@ from rasterio.rio.cli import cli
 @cli.command(short_help="Raster data calculator.")
 @click.argument('command')
 @files_inout_arg
+@click.option('--dtype', 
+              type=click.Choice(
+                  ['uint8', 'uint16', 'int16', 'float32', 'float64']),
+              help="Output data type.")
 @click.pass_context
-def calc(ctx, command, files):
+def calc(ctx, command, files, dtype):
+    """A raster data calculator
+
+    Applies one or more commands to a set of input datasets and writes the
+    results to a new dataset.
+
+    Command syntax is a work in progress.
+    """
     import numpy as np
 
     verbosity = (ctx.obj and ctx.obj.get('verbosity')) or 1
@@ -42,38 +53,67 @@ def calc(ctx, command, files):
 
                 parts = command.split(';')
                 if len(parts) == 1:
-                    with rasterio.open(output, 'w', **kwargs) as dst:
 
-                        # This one translates, eg, '{1}' to
-                        # 'sources[0]'.
-                        cmd = re.sub(
-                                r'{(\d)}',
-                                lambda m: 'sources[%d]' % (int(m.group(1))-1),
-                                parts.pop())
+                    # Translates, eg, '{1}' to 'sources[0]'.
+                    cmd = re.sub(
+                            r'{(\d)}',
+                            lambda m: 'sources[%d]' % (int(m.group(1))-1),
+                            parts.pop())
 
-                        logger.debug("Translated cmd: %r", cmd)
+                    logger.debug("Translated cmd: %r", cmd)
 
-                        # TODO: enable output dtype selection.
-                        results = eval(cmd).astype(kwargs['dtype'])
-                        dst.write(results)
+                    results = eval(cmd)
+                    
+                    # Using the class method instead of instance method.
+                    # Latter raises
+                    # TypeError: astype() got an unexpected keyword argument 'copy'
+                    # Possibly something to do with the instance being a masked
+                    # array.
+                    results = np.ndarray.astype(
+                            results, dtype or 'float64', copy=False)
+
+                    # Write results.
+                    if len(results.shape) == 3:
+                        kwargs.update(
+                                count=results.shape[0],
+                                dtype=results.dtype.type)
+                        with rasterio.open(output, 'w', **kwargs) as dst:
+                            dst.write(results)
+
+                    elif len(results.shape) == 2:
+                        kwargs.update(
+                                count=1,
+                                dtype=results.dtype.type)
+                        with rasterio.open(output, 'w', **kwargs) as dst:
+                            dst.write(results, 1)
 
                 else:
                     parts = list(filter(lambda p: p.strip(), parts))
                     kwargs['count'] = len(parts)
+
+                    results = []
+                    #with rasterio.open(output, 'w', **kwargs) as dst:
+
+                    for part in parts:
+                        cmd = re.sub(
+                                r'{(\d)\s*,\s*(\d)}',
+                                lambda m: 'sources[%d][%d]' % (
+                                    int(m.group(1))-1, int(m.group(2))-1),
+                                part)
+
+                        logger.debug("Translated cmd: %r", cmd)
+
+                        res = eval(cmd)
+                        res = np.ndarray.astype(
+                                res, dtype or 'float64', copy=False)
+                        results.append(res)
+
+                    results = np.asanyarray(results)
+                    kwargs.update(
+                            count=results.shape[0],
+                            dtype=results.dtype.type)
                     with rasterio.open(output, 'w', **kwargs) as dst:
-
-                        for i, part in enumerate(parts, 1):
-                            cmd = re.sub(
-                                    r'{(\d)\s*,\s*(\d)}',
-                                    lambda m: 'sources[%d][%d]' % (
-                                        int(m.group(1))-1, int(m.group(2))-1),
-                                    part)
-
-                            logger.debug("Translated cmd: %r", cmd)
-
-                            # TODO: enable output dtype selection.
-                            result = eval(cmd).astype(kwargs['dtype'])
-                            dst.write(result, i)
+                        dst.write(results)
 
         sys.exit(0)
     except Exception:
--- tests/test_rio_calc.py
+++ tests/test_rio_calc.py
@@ -10,7 +10,7 @@ from rasterio.rio.calc import calc
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 
-def test_all(tmpdir):
+def test_multiband_calc(tmpdir):
     outfile = str(tmpdir.join('out.tif'))
     runner = CliRunner()
     result = runner.invoke(calc, [
@@ -18,11 +18,12 @@ def test_all(tmpdir):
                 catch_exceptions=False)
     assert result.exit_code == 0
     with rasterio.open(outfile) as src:
+        assert src.meta['dtype'] == 'float64'
         data = src.read()
         assert data.min() == 125
 
 
-def test_bands(tmpdir):
+def test_singleband_calc(tmpdir):
     outfile = str(tmpdir.join('out.tif'))
     runner = CliRunner()
     result = runner.invoke(calc, [
@@ -30,5 +31,27 @@ def test_bands(tmpdir):
                 catch_exceptions=False)
     assert result.exit_code == 0
     with rasterio.open(outfile) as src:
+        assert src.meta['dtype'] == 'float64'
         data = src.read()
         assert data.min() == 125
+
+
+def test_parts_calc(tmpdir):
+    # Producing an RGB output from the hill shade.
+    # Red band has bumped up values. Other bands are unchanged.
+    outfile = str(tmpdir.join('out.tif'))
+    runner = CliRunner()
+    result = runner.invoke(calc, [
+                    '{1,1} + 125; {1,1}; {1,1}',
+                    '--dtype', 'uint8',
+                    'tests/data/shade.tif', 
+                    outfile],
+                catch_exceptions=False)
+    assert result.exit_code == 0
+    with rasterio.open(outfile) as src:
+        assert src.count == 3
+        assert src.meta['dtype'] == 'uint8'
+        data = src.read()
+        assert data[0].min() == 125
+        assert data[1].min() == 0
+        assert data[2].min() == 0
