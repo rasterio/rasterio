93a6e4017ca2f19cb90155b6a6fcc6cbf12a8619
Sean Gillies <sean@mapbox.com>
2015-02-09 10:30:21 -0700

    Remove redundant assertions.

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -11,9 +11,13 @@ def test_window():
 
 def test_window_transform():
     with rasterio.open('tests/data/RGB.byte.tif') as src:
-        assert src.window_transform(((0, src.height), (0, src.width))) == src.affine
         assert src.window_transform(((0, None), (0, None))) == src.affine
         assert src.window_transform(((None, None), (None, None))) == src.affine
-        assert src.window_transform(((None, src.height), (None, src.width))) == src.affine
-        assert src.window_transform(((1, None), (1, None))).c == src.bounds.left + src.res[0]
-        assert src.window_transform(((-1, None), (-1, None))).c == src.bounds.left - src.res[0]
+        assert src.window_transform(
+                ((1, None), (1, None))).c == src.bounds.left + src.res[0]
+        assert src.window_transform(
+                ((1, None), (1, None))).f == src.bounds.top - src.res[1]
+        assert src.window_transform(
+                ((-1, None), (-1, None))).c == src.bounds.left - src.res[0]
+        assert src.window_transform(
+                ((-1, None), (-1, None))).f == src.bounds.top + src.res[1]
