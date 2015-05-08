4f11b0cba28a954b75d5d89412f071fddaf83b8d
Sean Gillies <sean@mapbox.com>
2015-05-08 02:40:38 -0600

    Explain how to make a command plugin.
    Closes #348.

--- rasterio/rio/main.py
+++ rasterio/rio/main.py
@@ -1,10 +1,22 @@
-#!/usr/bin/env python
 # main: loader of all the command entry points.
 
 from pkg_resources import iter_entry_points
 
 from rasterio.rio.cli import cli
 
+
+# Find and load all entry points in the rasterio.rio_commands group.
+# This includes the standard commands included with Rasterio as well
+# as commands provided by other packages.
+#
+# At a mimimum, commands must use the rasterio.rio.cli.cli command
+# group decorator like so:
+#
+#   from rasterio.rio.cli import cli
+#
+#   @cli.command()
+#   def foo(...):
+#       ...
 
 for entry_point in iter_entry_points('rasterio.rio_commands'):
     entry_point.load()
