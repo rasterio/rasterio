0c188d9be23403638a864b5ba2a43c8eafd340ba
Sean Gillies <sean@mapbox.com>
2015-05-07 22:42:03 -0600

    Don't assign cli in main.py.

--- rasterio/rio/main.py
+++ rasterio/rio/main.py
@@ -3,8 +3,6 @@
 
 from pkg_resources import iter_entry_points
 
-from rasterio.rio.cli import cli
-
 
 for entry_point in iter_entry_points('rasterio.rio_commands'):
-    cli = entry_point.load()
+    entry_point.load()
