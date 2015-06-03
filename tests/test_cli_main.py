7941f29a0638a906ddc8578b09e39cb01b3e4805
Kevin Wurster <kevin@skytruth.org>
2015-06-03 16:59:41 -0400

    Added a test to verify that all of the subcommands listed in the entry-point rasterio.rio_commands are actually being properly registered.  Addresses a flaw in the testing strategy without having to change a bunch of tests.

--- tests/test_cli_main.py
+++ tests/test_cli_main.py
@@ -1,5 +1,6 @@
-from click.testing import CliRunner
+from pkg_resources import iter_entry_points
 
+from click.testing import CliRunner
 
 import rasterio
 from rasterio.rio.main import main_group
@@ -10,3 +11,11 @@ def test_version():
     result = runner.invoke(main_group, ['--version'])
     assert result.exit_code == 0
     assert rasterio.__version__ in result.output
+
+
+def test_all_registered():
+    # This test makes sure that all of the subcommands defined in the
+    # rasterio.rio_commands entry-point are actually registered to the main
+    # cli group.
+    for ep in iter_entry_points('rasterio.rio_commands'):
+        assert ep.name in main_group.commands
