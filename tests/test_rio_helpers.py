a5e70cb56c74b5bf67c123d490259300b9a3d446
Amit Kapadia <amit@planet.com>
2015-06-30 13:50:23 -0700

    test to_lower validation funct

--- tests/test_rio_helpers.py
+++ tests/test_rio_helpers.py
@@ -16,3 +16,8 @@ def test_resolve_files_inout__inout_files():
 def test_resolve_files_inout__inout_files_output_o():
     assert helpers.resolve_inout(
         files=('a', 'b', 'c'), output='out') == ('out', ['a', 'b', 'c'])
+
+
+def test_to_lower():
+    
+    assert helpers.to_lower(None, None, 'EPSG:3857') == 'epsg:3857'
