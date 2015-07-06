17b9a7f348aed1b3e6f6a2227304dd39ed110522
Sean Gillies <sean@mapbox.com>
2015-07-06 14:59:09 -0600

    Redo as a Cython property.
    Add docstrings.

--- rasterio/_base.pyx
+++ rasterio/_base.pyx
@@ -406,6 +406,7 @@ cdef class DatasetReader(object):
 
     @property
     def meta(self):
+        """The basic metadata of this dataset."""
         m = {
             'driver': self.driver,
             'dtype': self.dtypes[0],
@@ -423,15 +424,20 @@ cdef class DatasetReader(object):
         return m
 
 
-    @property
-    def profile(self):
-        m = self.meta
-        m.update(self.tags(ns='rio_creation_kwds'))
-        m.update(
-            blockxsize=self.block_shapes[0][1],
-            blockysize=self.block_shapes[0][0],
-            tiled=self.block_shapes[0][1] != self.width)
-        return m
+    property profile:
+        """Basic metadata and creation options of this dataset.
+
+        May be passed as keyword arguments to `rasterio.open()` to
+        create a clone of this dataset.
+        """
+        def __get__(self):
+            m = self.meta
+            m.update(self.tags(ns='rio_creation_kwds'))
+            m.update(
+                blockxsize=self.block_shapes[0][1],
+                blockysize=self.block_shapes[0][0],
+                tiled=self.block_shapes[0][1] != self.width)
+            return m
 
 
     def lnglat(self):
