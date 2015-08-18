66c592fde5dff66c323de1e297f78e513c4d3532
James McBride <jmcbride@berkeley.edu>
2015-08-17 21:41:55 -0700

    Merging function in python, called by rio-merge. The good: all tests pass, and it works nicely for my purposes.
    Some issues:
        + I suspect the community desires functionality on rasters, rather
          than on files, as written here. With what I've done so far, rio-merge
          becomes a very bare bones wrapper.
        + Since this is basically a wholesale move, I lost the rio debug
          information.
        + I do not really think features.py is the place for the python
          merge function, but was not sure of any good place.
        + Awkward "import as" to avoid merge name conflict.
        + Passing in resolution as a float or a single element tuple
          is not covered by tests (though does work in my testing).
    
    I'm taking the suggestion to make a PR at 30% (and not make too many
    about how others would like this to function) seriously though.

--- rasterio/features.py
+++ rasterio/features.py
@@ -2,6 +2,8 @@
 
 import json
 import logging
+import math
+import os
 import time
 import warnings
 
@@ -9,7 +11,7 @@ import numpy as np
 
 import rasterio
 from rasterio._features import _shapes, _sieve, _rasterize, _bounds
-from rasterio.transform import IDENTITY, guard_transform
+from rasterio.transform import IDENTITY, guard_transform, Affine
 from rasterio.dtypes import get_minimum_int_dtype
 
 
@@ -389,3 +391,165 @@ def bounds(geometry):
 
     geom = geometry.get('geometry') or geometry
     return _bounds(geom)
+
+
+def merge(files, output, driver, bounds=(), res=(), nodata=None,
+          verbosity=1, **creation_options):
+    """Copy valid pixels from input files to an output file.
+
+    All files must have the same number of bands, data type, and
+    coordinate reference system.
+
+    Input files are merged in their listed order using the reverse
+    painter's algorithm. If the output file exists, its values will be
+    overwritten by input values.
+
+    Geospatial bounds and resolution of a new output file in the
+    units of the input file coordinate reference system may be provided
+    and are otherwise taken from the first input file.
+
+    Parameters
+    ----------
+    files: list of strings
+        Filenames of files to be merged
+    output: str
+        Filename of merged imaged
+    driver: str
+        Specifies the output file type, such as "GTiff" or "JPEG".
+        See GDAL docs for more information.
+    bounds: tuple, optional 
+        Bounds of the output image (left, bottom, right, top).
+        If not set, bounds are determined from bounds of input files.
+    res: tuple, optional 
+        Output resolution in units of coordinate reference system. If not set,
+        the resolution of the first file is used. If a single value is passed,
+        output pixels will be square.
+    nodata: float, optional
+        nodata value to use in output file. If not set, uses the nodata value
+        in the input files
+    """
+
+    with rasterio.drivers(CPL_DEBUG=verbosity>2):
+        with rasterio.open(files[0]) as first:
+            first_res = first.res
+            kwargs = first.meta
+            kwargs.update(**creation_options)
+            kwargs.pop('affine')
+            nodataval = first.nodatavals[0]
+            dtype = first.dtypes[0]
+
+        if os.path.exists(output):
+            # TODO: prompt user to update existing file (-i option) like:
+            # overwrite b.tif? (y/n [n]) n
+            # not overwritten
+            dst = rasterio.open(output, 'r+')
+            nodataval = dst.nodatavals[0]
+            dtype = dst.dtypes[0]
+            dest = np.zeros((dst.count,) + dst.shape, dtype=dtype)
+        else:
+            # Create new output file.
+            # Extent from option or extent of all inputs.
+            if not bounds:
+                # scan input files.
+                xs = []
+                ys = []
+                for f in files:
+                    with rasterio.open(f) as src:
+                        left, bottom, right, top = src.bounds
+                        xs.extend([left, right])
+                        ys.extend([bottom, top])
+                bounds = min(xs), min(ys), max(xs), max(ys)
+            output_transform = Affine.translation(bounds[0], bounds[3])
+
+            # Resolution/pixel size.
+            if not res:
+                res = first_res
+            elif not np.iterable(res):
+                res = (res, res)
+            elif len(res) == 1:
+                res = (res[0], res[0])
+            output_transform *= Affine.scale(res[0], -res[1])
+
+            # Dataset shape.
+            output_width = int(math.ceil((bounds[2] - bounds[0]) / res[0]))
+            output_height = int(math.ceil((bounds[3] - bounds[1]) / res[1]))
+
+            kwargs['driver'] = driver
+            kwargs['transform'] = output_transform
+            kwargs['width'] = output_width
+            kwargs['height'] = output_height
+
+            dst = rasterio.open(output, 'w', **kwargs)
+            dest = np.zeros((first.count, output_height, output_width),
+                    dtype=dtype)
+
+        if nodata is not None:
+            nodataval = nodata
+
+        if nodataval is not None:
+            # Only fill if the nodataval is within dtype's range.
+            inrange = False
+            if np.dtype(dtype).kind in ('i', 'u'):
+                info = np.iinfo(dtype)
+                inrange = (info.min <= nodataval <= info.max)
+            elif np.dtype(dtype).kind == 'f':
+                info = np.finfo(dtype)
+                inrange = (info.min <= nodataval <= info.max)
+            if inrange:
+                dest.fill(nodataval)
+            else:
+                warnings.warn(
+                    "Input file's nodata value, %s, is beyond the valid "
+                    "range of its data type, %s. Consider overriding it "
+                    "using the --nodata option for better results." % (
+                        nodataval, dtype))
+        else:
+            nodataval = 0
+
+        dst_w, dst_s, dst_e, dst_n = dst.bounds
+
+        for fname in reversed(files):
+            with rasterio.open(fname) as src:
+                # Real World (tm) use of boundless reads.
+                # This approach uses the maximum amount of memory to solve
+                # the problem. Making it more efficient is a TODO.
+
+                # 1. Compute spatial intersection of destination
+                #    and source.
+                src_w, src_s, src_e, src_n = src.bounds
+
+                int_w = src_w if src_w > dst_w else dst_w
+                int_s = src_s if src_s > dst_s else dst_s
+                int_e = src_e if src_e < dst_e else dst_e
+                int_n = src_n if src_n < dst_n else dst_n
+
+                # 2. Compute the source window.
+                src_window = src.window(int_w, int_s, int_e, int_n)
+
+                # 3. Compute the destination window.
+                dst_window = dst.window(int_w, int_s, int_e, int_n)
+
+                # 4. Initialize temp array.
+                tsize = (first.count,) + tuple(b - a for a, b in dst_window)
+                temp = np.zeros(tsize, dtype=dtype)
+
+                temp = src.read(out=temp, window=src_window, boundless=False,
+                                masked=True)
+
+                # 5. Copy elements of temp into dest.
+                roff, coff = dst.index(int_w, int_n)
+                h, w = temp.shape[-2:]
+
+                region = dest[:,roff:roff+h,coff:coff+w]
+                np.copyto(region, temp,
+                    where=np.logical_and(
+                    region==nodataval, temp.mask==False))
+
+        if dst.mode == 'r+':
+            temp = dst.read(masked=True)
+            np.copyto(dest, temp,
+                where=np.logical_and(
+                dest==nodataval, temp.mask==False))
+
+        dst.write(dest)
+        dst.close()
