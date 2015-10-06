7fab760847ccedafe8f57bd4f6d38caf54eb2aca
Sean Gillies <sean@mapbox.com>
2015-10-06 11:09:51 -0600

    Update version and change log.

--- rasterio/__init__.py
+++ rasterio/__init__.py
@@ -23,7 +23,7 @@ from rasterio import _err, coords, enums
 
 __all__ = [
     'band', 'open', 'drivers', 'copy', 'pad']
-__version__ = "0.27.0"
+__version__ = "0.28.0"
 
 log = logging.getLogger('rasterio')
 class NullHandler(logging.Handler):
