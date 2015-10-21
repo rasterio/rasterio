b676671056564cf76ef175a0d83ba0163f1368dc
Sean Gillies <sean@mapbox.com>
2015-10-20 22:39:36 -0600

    Silence tar and make

--- scripts/travis_gdal_install.sh
+++ scripts/travis_gdal_install.sh
@@ -60,30 +60,30 @@ ls -l $GDALINST
 if [ ! -d $GDALINST/gdal-1.9.2 ]; then
   cd $GDALBUILD
   wget http://download.osgeo.org/gdal/gdal-1.9.2.tar.gz
-  tar -xzvf gdal-1.9.2.tar.gz
+  tar -xzf gdal-1.9.2.tar.gz
   cd gdal-1.9.2
   ./configure --prefix=$GDALINST/gdal-1.9.2 $GDALOPTS
-  make -j 2
+  make -s -j 2
   make install
 fi
 
 if [ ! -d $GDALINST/gdal-1.11.2 ]; then
   cd $GDALBUILD
   wget http://download.osgeo.org/gdal/1.11.2/gdal-1.11.2.tar.gz
-  tar -xzvf gdal-1.11.2.tar.gz
+  tar -xzf gdal-1.11.2.tar.gz
   cd gdal-1.11.2
   ./configure --prefix=$GDALINST/gdal-1.11.2 $GDALOPTS
-  make -j 2
+  make -s -j 2
   make install
 fi
 
 if [ ! -d $GDALINST/gdal-2.0.1 ]; then
   cd $GDALBUILD
   wget http://download.osgeo.org/gdal/2.0.1/gdal-2.0.1.tar.gz
-  tar -xzvf gdal-2.0.1.tar.gz
+  tar -xzf gdal-2.0.1.tar.gz
   cd gdal-2.0.1
   ./configure --prefix=$GDALINST/gdal-2.0.1 $GDALOPTS
-  make -j 2
+  make -s -j 2
   make install
 fi
 
