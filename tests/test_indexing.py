7e69b225d450395d9111a9655421d76f5c314f60
Brendan Ward <bcward@consbio.org>
2015-10-22 22:51:04 -0700

    Add more windows utilities: get_data_window, window_union, window_intersection, windows_intersect

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -1,4 +1,13 @@
+import numpy
+import pytest
+
 import rasterio
+from rasterio import (
+    get_data_window, window_intersection, window_union, windows_intersect
+)
+
+
+DATA_WINDOW = ((3, 5), (2, 6))
 
 
 def test_index():
@@ -67,3 +76,119 @@ def test_window_full_cover():
         win = src.window(*bounds)
         bounds_calc = list(src.window_bounds(win))
         assert bound_covers(bounds_calc, bounds)
+
+
+@pytest.fixture
+def data():
+    data = numpy.zeros((10, 10), dtype='uint8')
+    data[slice(*DATA_WINDOW[0]), slice(*DATA_WINDOW[1])] = 1
+    return data
+
+
+def test_data_window_unmasked(data):
+    window = get_data_window(data)
+    assert window == ((0, data.shape[0]), (0, data.shape[1]))
+
+
+def test_data_window_masked(data):
+    data = numpy.ma.masked_array(data, data == 0)
+    window = get_data_window(data)
+    assert window == DATA_WINDOW
+
+
+def test_data_window_nodata(data):
+    window = get_data_window(data, nodata=0)
+    assert window == DATA_WINDOW
+
+    window = get_data_window(numpy.ones_like(data), nodata=0)
+    assert window == ((0, data.shape[0]), (0, data.shape[1]))
+
+
+def test_data_window_nodata_disjunct():
+    data = numpy.zeros((3, 10, 10), dtype='uint8')
+    data[0, :4, 1:4] = 1
+    data[1, 2:5, 2:8] = 1
+    data[2, 1:6, 1:6] = 1
+    window = get_data_window(data, nodata=0)
+    assert window == ((0, 6), (1, 8))
+
+
+def test_data_window_empty_result():
+    data = numpy.zeros((3, 10, 10), dtype='uint8')
+    window = get_data_window(data, nodata=0)
+    assert window == ((0, 0), (0, 0))
+
+
+def test_data_window_masked_file():
+    with rasterio.open('tests/data/RGB.byte.tif') as src:
+        window = get_data_window(src.read(1, masked=True))
+        assert window == ((3, 714), (13, 770))
+
+        window = get_data_window(src.read(masked=True))
+        assert window == ((3, 714), (13, 770))
+
+
+def test_window_union():
+    assert window_union([
+        ((0, 6), (3, 6)),
+        ((2, 4), (1, 5))
+    ]) == ((0, 6), (1, 6))
+
+
+def test_window_intersection():
+    assert window_intersection([
+        ((0, 6), (3, 6)),
+        ((2, 4), (1, 5))
+    ]) == ((2, 4), (3, 5))
+
+    assert window_intersection([
+        ((0, 6), (3, 6)),
+        ((6, 10), (1, 5))
+    ]) == ((6, 6), (3, 5))
+
+    assert window_intersection([
+        ((0, 6), (3, 6)),
+        ((2, 4), (1, 5)),
+        ((3, 6), (0, 6))
+    ]) == ((3, 4), (3, 5))
+
+
+def test_window_intersection_disjunct():
+    with pytest.raises(ValueError):
+        window_intersection([
+            ((0, 6), (3, 6)),
+            ((100, 200), (0, 12)),
+            ((7, 12), (7, 12))
+        ])
+
+
+def test_windows_intersect():
+    assert windows_intersect([
+        ((0, 6), (3, 6)),
+        ((2, 4), (1, 5))
+    ]) == True
+
+    assert windows_intersect([
+        ((0, 6), (3, 6)),
+        ((2, 4), (1, 5)),
+        ((3, 6), (0, 6))
+    ]) == True
+
+
+def test_windows_intersect_disjunct():
+    assert windows_intersect([
+        ((0, 6), (3, 6)),
+        ((10, 20), (0, 6))
+    ]) == False
+
+    assert windows_intersect([
+        ((0, 6), (3, 6)),
+        ((2, 4), (1, 5)),
+        ((5, 6), (0, 6))
+    ]) == False
+
+    assert windows_intersect([
+        ((0, 6), (3, 6)),
+        ((2, 4), (1, 3)),
+        ((3, 6), (4, 6))
+    ]) == False
