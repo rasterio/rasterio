9fbc22b754f2462ab6374ce24445a0028cdad940
Sean Gillies <sean@mapbox.com>
2016-03-22 15:41:53 -0600

    Add tests to shake up warp transformer creation
    Also rename an error class
    
    Closes #595

--- rasterio/errors.py
+++ rasterio/errors.py
@@ -7,7 +7,7 @@ class RasterioIOError(IOError):
     """A failure to open a dataset using the presently registered drivers."""
 
 
-class RasterioDriverRegistrationError(ValueError):
+class DriverRegistrationError(ValueError):
     """To be raised when, eg, _gdal.GDALGetDriverByName("MEM") returns NULL."""
 
 
--- tests/test_warp_transform.py
+++ tests/test_warp_transform.py
@@ -3,7 +3,7 @@ from rasterio._warp import _calculate_default_transform
 from rasterio.transform import Affine, from_bounds
 
 
-def test_indentity():
+def test_identity():
     """Get the same transform and dimensions back for same crs."""
     # Tile: [53, 96, 8]
     # [-11740727.544603072, 4852834.0517692715, -11584184.510675032, 5009377.085697309]
@@ -36,3 +36,28 @@ def test_gdal_transform_notnull():
             right=-80,
             top=70)
     assert True
+
+
+def test_gdal_transform_fail_dst_crs():
+    with rasterio.drivers():
+        dt, dw, dh = _calculate_default_transform(
+            {'init': 'EPSG:4326'},
+            '+proj=foobar',
+            width=80,
+            height=80,
+            left=-120,
+            bottom=30,
+            right=-80,
+            top=70)
+
+def test_gdal_transform_fail_src_crs():
+    with rasterio.drivers():
+        dt, dw, dh = _calculate_default_transform(
+            '+proj=foobar',
+            {'init': 'EPSG:32610'},
+            width=80,
+            height=80,
+            left=-120,
+            bottom=30,
+            right=-80,
+            top=70)
