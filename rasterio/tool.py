871e71928b499e51c081cf9ea349d5dd21a5b9e5
Matthew Perry <perrygeo@gmail.com>
2016-04-07 12:15:21 -0400

    tweak tool.py imports

--- rasterio/tool.py
+++ rasterio/tool.py
@@ -2,28 +2,31 @@
 DEPRECATED; To be removed in 1.0
 """
 from __future__ import absolute_import
-import rasterio.rio.insp
 
 
 def show(*args, **kwargs):
+    import rasterio.plot
     import warnings
     warnings.warn("Deprecated; Use rasterio.rio.insp instead", DeprecationWarning)
-    return rasterio.rio.insp.show(*args, **kwargs)
+    return rasterio.plot.show_hist(*args, **kwargs)
+
+
+def show_hist(*args, **kwargs):
+    import rasterio.plot
+    import warnings
+    warnings.warn("Deprecated; Use rasterio.rio.insp instead", DeprecationWarning)
+    return rasterio.plot.show_hist(*args, **kwargs)
 
 
 def stats(*args, **kwargs):
+    import rasterio.rio.insp
     import warnings
     warnings.warn("Deprecated; Use rasterio.rio.insp instead", DeprecationWarning)
     return rasterio.rio.insp.stats(*args, **kwargs)
 
 
-def show_hist(*args, **kwargs):
-    import warnings
-    warnings.warn("Deprecated; Use rasterio.rio.insp instead", DeprecationWarning)
-    return rasterio.rio.insp.show_hist(*args, **kwargs)
-
-
 def main(*args, **kwargs):  # pragma: no cover
+    import rasterio.rio.insp
     import warnings
     warnings.warn("Deprecated; Use rasterio.rio.insp instead", DeprecationWarning)
     return rasterio.rio.insp.main(*args, **kwargs)
