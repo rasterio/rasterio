431d06a6b7cf8946a3f0910e766438b41503871b
Erik Seglem <erik.seglem@gmail.com>
2016-04-08 21:21:39 -0400

    Minor formatting update.
    Does str really need a docstring?

--- rasterio/warnings.py
+++ rasterio/warnings.py
@@ -2,7 +2,8 @@
 
 
 class NodataShadowWarning(Warning):
-    """Warn that a dataset's nodata attribute is shadowing its alpha band"""
+    """Warn that a dataset's nodata attribute is shadowing its alpha band."""
+
     def __str__(self):
         return ("The dataset's nodata attribute is shadowing "
                 "the alpha band. All masks will be determined "
