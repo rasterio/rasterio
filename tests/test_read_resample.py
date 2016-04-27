ff7108566d156612b99541d615c78b0bf65b9c43
Matthew Perry <perrygeo@gmail.com>
2016-04-27 13:05:41 -0400

    add failing testcase to demonstrate #679

--- tests/test_read_resample.py
+++ tests/test_read_resample.py
@@ -32,3 +32,17 @@ def test_read_out_shape_resample_up():
         data = s.read(1, out=out, masked=True)
         assert data.shape == (7180, 7910)
         assert data.mean() == s.read(1, masked=True).mean()
+
+
+def test_read_downsample_alpha():
+    with rasterio.open('tests/data/alpha.tif') as src:
+        out = numpy.zeros((19, 19), dtype=rasterio.ubyte)
+        assert src.width == 1223
+        assert src.height == 1223
+        assert src.count == 4
+
+        # attempt ~ 1/64 decimated read of red band (works)
+        assert src.read(1, out=out, masked=False).shape == out.shape
+
+        # attempt ~ 1/64 decimated read of alpha band (segfault?)
+        src.read(4, out=out, masked=False)
