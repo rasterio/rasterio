5c3bd2ebdfd1abdacc0e2603fbba0f4e5b3eb5e3
Colin Talbert <talbertc@usgs.gov>
2016-05-24 10:16:06 -0600

    Update reshape_image test for new decoupled io function

--- tests/test_reshape_image.py
+++ tests/test_reshape_image.py
@@ -3,7 +3,7 @@ import rasterio
 
 def test_reshape():
     with rasterio.open('tests/data/RGB.byte.tif') as src:
-        im_data = rasterio.plot.reshape_as_image(src)
+        im_data = rasterio.plot.reshape_as_image(src.read())
         assert im_data.shape == (718, 791, 3)
 
 def test_toundtrip_reshape():
