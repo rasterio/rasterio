bbd3346b8d1e7420c5a4fc751266ad79dab69261
Matthew Perry <perrygeo@gmail.com>
2016-06-02 14:43:10 -0400

    make sure transforms are affine instances

--- rasterio/warp.py
+++ rasterio/warp.py
@@ -247,9 +247,9 @@ def reproject(
     # If working with identity transform, assume it is crs-less data
     # and that translating the matrix very slightly will avoid #674
     eps = 1e-100
-    if src_transform and src_transform.is_identity:
+    if src_transform and guard_transform(src_transform).is_identity:
         src_transform = src_transform.translation(eps, eps)
-    if dst_transform and dst_transform.is_identity:
+    if dst_transform and guard_transform(dst_transform).is_identity:
         dst_transform = dst_transform.translation(eps, eps)
 
     if src_transform:
