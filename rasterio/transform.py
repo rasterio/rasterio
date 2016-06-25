5a82b3693dedb2fa8352b8fa50df126659fed96a
Kevin Wurster <wursterk@gmail.com>
2016-06-25 00:16:37 -0400

    Function for converting between (row, col) and (x, y).  User determines if the returned coordinates are for the pixel center or a corner.

--- rasterio/transform.py
+++ rasterio/transform.py
@@ -61,3 +61,42 @@ def array_bounds(height, width, transform):
     w, n = transform.xoff, transform.yoff
     e, s = transform * (width, height)
     return w, s, e, n
+
+
+def xy(row, col, transform, offset='center'):
+    """Returns the coordinates ``(x, y)`` of a pixel at `row` and `col`.
+    The pixel's center is returned by default, but a corner can be returned
+    by setting `offset` to one of `ul, ur, ll, lr`.
+
+    Parameters
+    ----------
+    row : int
+        Pixel row.
+    col : int
+        Pixel column.
+    transform : affine.Affine
+        Transformation from pixel coordinates to coordinate reference system.
+    offset : str, optional
+        Determines if the returned coordinates are for the center of the
+        pixel or for a corner.
+
+    Returns
+    -------
+    tuple
+        ``(x, y)``
+    """
+
+    rc = (row, col)
+
+    if offset == 'center':
+        return transform * transform.translation(0.5, 0.5) * rc
+    elif offset == 'ul':
+        return transform * rc
+    elif offset == 'ur':
+        return transform * transform.translation(1, 0) * rc
+    elif offset == 'll':
+        return transform * transform.translation(0, 1) * rc
+    elif offset == 'lr':
+        return transform * transform.translation(1, 1) * rc
+    else:
+        raise ValueError("Invalid offset")
