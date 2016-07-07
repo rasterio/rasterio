7d067f6024b5d8f33c802fb3af8173b342232176
Sean Gillies <sean@mapbox.com>
2016-07-07 16:21:52 -0600

    New affine/transform safe profile class
    Resolves #823

--- rasterio/profiles.py
+++ rasterio/profiles.py
@@ -1,41 +1,66 @@
 """Raster dataset profiles."""
 
+from collections import UserDict
+import warnings
+
 from rasterio.dtypes import uint8
 
 
-class Profile:
+class Profile(UserDict):
     """Base class for Rasterio dataset profiles.
 
     Subclasses will declare a format driver and driver-specific
     creation options.
     """
 
-    driver = None
     defaults = {}
 
-    def __call__(self, **kwargs):
-        """Return a mapping of keyword args for writing a new datasets.
+    def __init__(self, data={}, **kwds):
+        UserDict.__init__(self)
+        initdata = self.defaults.copy()
+        initdata.update(data)
+        initdata.update(**kwds)
 
-        Example:
+        if 'affine' in initdata and 'transform' in initdata:
+            raise TypeError("affine and transform cannot both be specified")
+        elif 'affine' in initdata:
+            warnings.warn("affine item is deprecated, use transform instead",
+                          DeprecationWarning)
+            initdata['transform'] = initdata.pop('affine')
 
-            profile = SomeProfile()
-            with rasterio.open('foo.tif', 'w', **profile()) as dst:
-                # Write data ...
+        self.data.update(initdata)
+
+    def __getitem__(self, key):
+        """Like normal item access but with affine alias."""
+        if key == 'affine':
+            key = 'transform'
+            warnings.warn("affine item is deprecated, use transform instead",
+                          DeprecationWarning)
+        return self.data[key]
+
+    def __setitem__(self, key, val):
+        """Like normal item setter but forbidding affine item."""
+        if key == 'affine':
+            raise TypeError("affine key is prohibited")
+        self.data[key] = val
+
+    def __call__(self, **kwds):
+        """Return a mapping of keyword args.
+
+        DEPRECATED.
         """
-        if kwargs.get('driver', self.driver) != self.driver:
-            raise ValueError(
-                "Overriding this profile's driver is not allowed.")
-        profile = self.defaults.copy()
-        profile.update(**kwargs)
-        profile['driver'] = self.driver
+        warnings.warn("__call__() is deprecated, use mapping methods instead",
+                      DeprecationWarning)
+        profile = self.data.copy()
+        profile.update(**kwds)
         return profile
 
 
 class DefaultGTiffProfile(Profile):
-    """A tiled, band-interleaved, LZW-compressed, 8-bit GTiff profile."""
+    """Tiled, band-interleaved, LZW-compressed, 8-bit GTiff."""
 
-    driver = 'GTiff'
     defaults = {
+        'driver': 'GTiff',
         'interleave': 'band',
         'tiled': True,
         'blockxsize': 256,
