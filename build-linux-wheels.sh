f7dda743c789ee70f84774c273365f96eddfa654
Sean Gillies <sean@mapbox.com>
2016-09-23 14:11:56 +0200

    Add Makefile and GDAL 2.1.1
    Resolves #894

--- build-linux-wheels.sh
+++ build-linux-wheels.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -eu
 
-# Checking for /.dockerenv is a hacky way to determine whether or not we're
-# already running in a Docker container. Note that this is not guaranteed to
-# exist in all versions and drivers and may need to be changed later.
-if [ ! -e /.dockerenv ]; then
-    docker build -f Dockerfile.wheels -t rasterio-wheelbuilder .
-    exec docker run -v `pwd`:/io rasterio-wheelbuilder "$@"
-fi
-
 ORIGINAL_PATH=$PATH
 UNREPAIRED_WHEELS=/tmp/wheels
 
--- setup.py
+++ setup.py
@@ -249,7 +249,6 @@ setup_args = dict(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: C',
         'Programming Language :: Cython',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
