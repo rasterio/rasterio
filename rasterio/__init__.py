82f64d8e84009e01ba554c9b14d5bfbafd427801
Sean Gillies <sean@mapbox.com>
2017-02-14 15:30:19 +0100

    Update version and change log

--- rasterio/__init__.py
+++ rasterio/__init__.py
@@ -54,7 +54,7 @@ def window_index(*args, **kwargs):
 
 __all__ = [
     'band', 'open', 'copy', 'pad']
-__version__ = "1.0a6"
+__version__ = "1.0a7"
 __gdal_version__ = gdal_version()
 
 # Rasterio attaches NullHandler to the 'rasterio' logger and its
