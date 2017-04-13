6b45ad07a4cfc01b230b5ec56720ba2f2e9ae474
Kevin Wurster <wursterk@gmail.com>
2017-04-13 19:41:34 -0400

    Remove unnecessary 'rasterio.Env()' calls.

--- tests/test_warp_transform.py
+++ tests/test_warp_transform.py
@@ -2,7 +2,6 @@ import os
 
 import pytest
 
-import rasterio
 from rasterio._warp import _calculate_default_transform
 from rasterio.control import GroundControlPoint
 from rasterio.errors import CRSError
@@ -34,9 +33,8 @@ def test_identity():
         5009377.085697309)
     transform = from_bounds(left, bottom, right, top, width, height)
 
-    with rasterio.Env():
-        res_transform, res_width, res_height = _calculate_default_transform(
-            src_crs, dst_crs, width, height, left, bottom, right, top)
+    res_transform, res_width, res_height = _calculate_default_transform(
+        src_crs, dst_crs, width, height, left, bottom, right, top)
 
     assert res_width == width
     assert res_height == height
@@ -84,16 +82,15 @@ def test_transform_bounds():
 
 
 def test_gdal_transform_notnull():
-    with rasterio.Env():
-        dt, dw, dh = _calculate_default_transform(
-            src_crs={'init': 'EPSG:4326'},
-            dst_crs={'init': 'EPSG:32610'},
-            width=80,
-            height=80,
-            left=-120,
-            bottom=30,
-            right=-80,
-            top=70)
+    dt, dw, dh = _calculate_default_transform(
+        src_crs={'init': 'EPSG:4326'},
+        dst_crs={'init': 'EPSG:32610'},
+        width=80,
+        height=80,
+        left=-120,
+        bottom=30,
+        right=-80,
+        top=70)
     assert True
 
 
@@ -127,17 +124,16 @@ def test_gdal_transform_fail_src_crs():
     os.environ.get('GDALVERSION', 'a.b.c').startswith('1.9'),
     reason="GDAL 1.9 doesn't catch this error")
 def test_gdal_transform_fail_dst_crs_xfail():
-    with rasterio.Env():
-        with pytest.raises(CRSError):
-            dt, dw, dh = _calculate_default_transform(
-                {'init': 'EPSG:4326'},
-                {'proj': 'foobar'},
-                width=80,
-                height=80,
-                left=-120,
-                bottom=30,
-                right=-80,
-                top=70)
+    with pytest.raises(CRSError):
+        dt, dw, dh = _calculate_default_transform(
+            {'init': 'EPSG:4326'},
+            {'proj': 'foobar'},
+            width=80,
+            height=80,
+            left=-120,
+            bottom=30,
+            right=-80,
+            top=70)
 
 
 def test_gcps_calculate_transform():
