1e5ce2896def27ba3228b092dea425cf018c8b05
Kevin Wurster <wursterk@gmail.com>
2017-05-10 20:42:05 -0400

    Simplify test.

--- tests/test_copy.py
+++ tests/test_copy.py
@@ -1,28 +1,10 @@
-import logging
-import os.path
-import unittest
-import shutil
-import subprocess
-import sys
-import tempfile
-
 import rasterio
 
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
-
-class CopyTest(unittest.TestCase):
-
-    def setUp(self):
-        self.tempdir = tempfile.mkdtemp()
-
-    def tearDown(self):
-        shutil.rmtree(self.tempdir)
-
-    def test_copy(self):
-        name = os.path.join(self.tempdir, 'test_copy.tif')
-        rasterio.copy(
-            'tests/data/RGB.byte.tif',
-            name)
-        info = subprocess.check_output(["gdalinfo", name])
-        self.assert_("GTiff" in info.decode('utf-8'))
+def test_copy(tmpdir, path_rgb_byte_tif):
+    outfile = str(tmpdir.join('test_copy.tif'))
+    rasterio.copy(
+        path_rgb_byte_tif,
+        outfile)
+    with rasterio.open(outfile) as src:
+        assert src.driver == 'GTiff'
