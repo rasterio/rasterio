3bc5b5b8ca1d60dd4ed4f7bd37fd74dd93784124
Sean Gillies <sean@mapbox.com>
2017-06-01 17:52:10 +0200

    Switch to pytest.warns()

--- tests/test_deprecated.py
+++ tests/test_deprecated.py
@@ -1,8 +1,6 @@
 """Unittests for deprecated features"""
 
 
-import warnings
-
 import affine
 import pytest
 
@@ -16,17 +14,16 @@ def test_open_affine_and_transform(path_rgb_byte_tif):
     By settings the 'affine' kwarg to a wacky value we ensure that the
     'transform' kwarg is used while ignoring the 'affine' kwarg.
     """
-    with warnings.catch_warnings(record=True) as w:
+    with pytest.warns(None) as record:
         with rasterio.open(
                 path_rgb_byte_tif,
                 affine=rasterio,
                 transform=affine.Affine.identity()) as src:
             pass
-        assert len(w) == 2
-        assert "'affine' kwarg in rasterio.open() is deprecated" \
-               in str(w[0].message)
-        assert "Found both 'affine' and 'transform'" in str(w[1].message)
-        assert "choosing 'transform'" in str(w[1].message)
+        assert len(record) == 2
+        assert "The 'affine' kwarg in rasterio.open() is deprecated" in str(record[0].message)
+        assert "choosing 'transform'" in str(record[1].message)
+
 
 
 def test_open_transform_gdal_geotransform(path_rgb_byte_tif):
