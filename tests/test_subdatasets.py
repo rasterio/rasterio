37273f8428ba10049ff576a6906d2982232530b9
Sean Gillies <sean@mapbox.com>
2017-09-18 11:58:43 -0600

    Skip netcdf test for GDAL < 2.1

--- tests/test_subdatasets.py
+++ tests/test_subdatasets.py
@@ -1,7 +1,13 @@
+import pytest
+
 import rasterio
 
 
+@pytest.mark.skipif(rasterio.__gdal_version__.startswith('1.9') or
+                    rasterio.__gdal_version__.startswith('2.0'),
+                    reason="netcdf driver not available before GDAL 2.1")
 def test_subdatasets():
+    """Get subdataset names and descriptions"""
     with rasterio.open('netcdf:tests/data/RGB.nc') as src:
         subs = src.subdatasets()
         assert len(subs) == 3
