76d225bba1308bc59b7006042e2fcfda0f91e2af
Sean Gillies <sean@mapbox.com>
2017-10-06 12:33:23 -0600

    Remove test that can't fail unless we run out of memory

--- tests/test_warp.py
+++ tests/test_warp.py
@@ -265,24 +265,6 @@ def test_calculate_default_transform_multiple_resolutions():
         assert height == 20
 
 
-def test_calculate_default_transform_large_dimensions():
-    target_transform = Affine(0.027830579216425146, 0, 0, 0,
-                              -0.027830579216425146, 111325.14286638486)
-
-    src_crs = CRS.from_epsg(4326)
-    dst_crs = CRS.from_epsg(3857)
-    src_width = 4000000
-    src_height = 4000000
-    src_bounds = (0, 0, 1, 1)
-
-    dst_transform, width, height = calculate_default_transform(
-        src_crs, dst_crs, src_width, src_height, *src_bounds)
-
-    assert dst_transform.almost_equals(target_transform)
-    assert width == 3999898
-    assert height == 4000102
-
-
 def test_reproject_ndarray():
     with rasterio.open('tests/data/RGB.byte.tif') as src:
         source = src.read(1)
