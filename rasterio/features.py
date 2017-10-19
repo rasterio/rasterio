79062b42a106d90ec506d2eac25d7e18331e965d
Brendan Ward <brendan-ward@users.noreply.github.com>
2017-10-19 06:48:45 -0700

    Prevent segfaults in rasterize (#1176)
    * Add better check for GeoJSON inputs to rasterize and associated tests
    * Restructured OGRGeomBuilder to avoid segfault

--- rasterio/features.py
+++ rasterio/features.py
@@ -239,15 +239,14 @@ def rasterize(
             value = default_value
         geom = getattr(geom, '__geo_interface__', None) or geom
 
-        # not isinstance(geom, dict) or
-        if 'type' in geom or 'coordinates' in geom:
-            valid_shapes.append((geom, value))
-            shape_values.append(value)
-
-        else:
+        # geom must be a valid GeoJSON geometry type and non-empty
+        if not is_valid_geom(geom):
             raise ValueError(
                 'Invalid geometry object at index {0}'.format(index)
             )
+
+        valid_shapes.append((geom, value))
+        shape_values.append(value)
 
     if not valid_shapes:
         raise ValueError('No valid geometry objects found for rasterize')
@@ -369,3 +368,86 @@ def geometry_window(raster, shapes, pad_x=0, pad_y=0, north_up=True,
     window = window.intersection(raster_window)
 
     return window
+
+
+def is_valid_geom(geom):
+    """
+    Checks to see if geometry is a valid GeoJSON geometry type or
+    GeometryCollection.
+
+    Geometries must be non-empty, and have at least x, y coordinates.
+
+    Note: only the first coordinate is checked for validity.
+    
+    Parameters
+    ----------
+    geom: an object that implements the geo interface or GeoJSON-like object
+
+    Returns
+    -------
+    bool: True if object is a valid GeoJSON geometry type
+    """
+    
+    geom_types = {'Point', 'MultiPoint', 'LineString', 'MultiLineString',
+                  'Polygon', 'MultiPolygon'}
+
+    if 'type' not in geom:
+        return False
+
+    try:
+        geom_type = geom['type']
+        if geom_type not in geom_types.union({'GeometryCollection'}):
+            return False
+
+    except TypeError:
+        return False
+
+    if geom_type in geom_types:
+        if 'coordinates' not in geom:
+            return False
+
+        coords = geom['coordinates']
+
+        if geom_type == 'Point':
+            # Points must have at least x, y
+            return len(coords) >= 2
+
+        if geom_type == 'MultiPoint':
+            # Multi points must have at least one point with at least x, y
+            return len(coords) > 0 and len(coords[0]) >= 2
+
+        if geom_type == 'LineString':
+            # Lines must have at least 2 coordinates and at least x, y for
+            # a coordinate
+            return len(coords) >= 2 and len(coords[0]) >= 2
+
+        if geom_type == 'MultiLineString':
+            # Multi lines must have at least one LineString
+            return (len(coords) > 0 and len(coords[0]) >= 2 and
+                    len(coords[0][0]) >=2)
+
+        if geom_type == 'Polygon':
+            # Polygons must have at least 1 ring, with at least 4 coordinates,
+            # with at least x, y for a coordinate
+            return (len(coords) > 0 and len(coords[0]) >= 4 and
+                    len(coords[0][0]) >=2)
+
+        if geom_type == 'MultiPolygon':
+            # Muti polygons must have at least one Polygon
+            return (len(coords) > 0 and len(coords[0]) > 0 and
+                    len(coords[0][0]) >= 4 and len(coords[0][0][0]) >=2)
+
+    if geom_type == 'GeometryCollection':
+        if not 'geometries' in geom:
+            return False
+
+        if not len(geom['geometries']) > 0:
+            # While technically valid according to GeoJSON spec, an empty
+            # GeometryCollection will cause issues if used in rasterio
+            return False
+
+        for g in geom['geometries']:
+            if not is_valid_geom(g):
+                return False  # short-circuit and fail early
+
+    return True
