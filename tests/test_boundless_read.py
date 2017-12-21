cddb90c3206ec16e953eaec0dce34271f0d4f8c4
Sean C. Gillies <sean@mapbox.com>
2017-12-21 09:02:18 -0700

    Add test of pixel fidelity

---
+++ tests/test_boundless_read.py
@@ -0,0 +1,14 @@
+"""Test of boundless reads"""
+
+import numpy
+
+import rasterio
+from rasterio.windows import Window
+
+
+def test_pixel_fidelity(path_rgb_byte_tif):
+    with rasterio.open(path_rgb_byte_tif) as dataset:
+        rgb = dataset.read()
+        rgb_padded = dataset.read(window=Window(-100, -100, dataset.width + 200, dataset.height + 200), boundless=True)
+
+    assert numpy.all(rgb == rgb_padded[:, 100:-100, 100:-100])
