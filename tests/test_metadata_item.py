aa88ad2776ed7f5562dba3aaf170945f297bf75a
vincentsarago <vincent.sarago@mapbox.com>
2018-04-04 13:00:25 -0400

    add tests

--- tests/test_metadata_item.py
+++ tests/test_metadata_item.py
@@ -12,3 +12,4 @@ def test_get_metadata_item():
     with rasterio.open('tests/data/cogeo.tif') as src:
         assert src.get_metadata_item(1, 'IFD_OFFSET', 'TIFF') == '8'
         assert src.get_metadata_item(1, 'IFD_OFFSET', 'TIFF', ovr=1) == '1504'
+        assert not src.get_metadata_item(1, 'IF', 'TIFF')
