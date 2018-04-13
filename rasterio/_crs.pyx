3b4ff61b5ff991beafcc02b99e70ace2d2962a0b
Sean Gillies <sean.gillies@gmail.com>
2018-04-13 16:40:26 -0600

    Clean up logic

--- rasterio/_crs.pyx
+++ rasterio/_crs.pyx
@@ -50,23 +50,22 @@ class _CRS(UserDict):
         cdef int retval
 
         try:
-          if not self and not other:
-              warnings.warn("Comparing empty CRS Objects. Will return True")
-
-          # use dictionary equality rules first
-          if UserDict(self.data) == UserDict(other):
-              return True
-          else:
             if not self and not other:
                 return True
 
-            if not self or not other:
+            # use dictionary equality rules first
+            elif UserDict(self.data) == UserDict(other):
+                return True
+
+            elif not self or not other:
                 return False
 
-            osr_crs1 = osr_from_crs(self)
-            osr_crs2 = osr_from_crs(other)
-            retval = OSRIsSame(osr_crs1, osr_crs2)
-            return bool(retval == 1)
+            else:
+                osr_crs1 = osr_from_crs(self)
+                osr_crs2 = osr_from_crs(other)
+                retval = OSRIsSame(osr_crs1, osr_crs2)
+                return bool(retval == 1)
+
         finally:
             _safe_osr_release(osr_crs1)
             _safe_osr_release(osr_crs2)
