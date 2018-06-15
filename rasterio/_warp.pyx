6359cff8af7a062250433bd7bb72d23ef5027f15
Sean C. Gillies <sean@mapbox.com>
2018-06-15 15:21:31 -0600

    Revert other change from 73ca8cf

--- rasterio/_warp.pyx
+++ rasterio/_warp.pyx
@@ -931,8 +931,8 @@ cdef class WarpedVRTReaderBase(DatasetReaderBase):
         self._nodatavals = [
             self.dst_nodata for i in src_dataset.indexes]
 
-        if self.dst_alpha and len(self._nodatavals) == 3:
-            self._nodatavals.append(None)
+        # if self.dst_alpha and len(self._nodatavals) == 3:
+        #     self._nodatavals.append(None)
 
     def get_crs(self):
         warnings.warn("get_crs() will be removed in 1.0", RasterioDeprecationWarning)
