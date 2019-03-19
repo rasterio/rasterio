afe4a210af3c3755886962157fd70ce727eb9bc4
Sean Gillies <sean@mapbox.com>
2019-03-19 17:58:43 -0600

    Resolves #1651 and #1654

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -2,10 +2,15 @@
 
 
 import os
+try:
+    from unittest.mock import MagicMock
+except ImportError:
+    from mock import MagicMock
 
 import pytest
 
 import rasterio
+from rasterio.enums import Compression
 from rasterio.errors import RasterioIOError
 
 
@@ -26,3 +31,12 @@ def test_handle_closed(path_rgb_byte_tif):
         pass
     with pytest.raises(RasterioIOError):
         src.files
+
+
+@pytest.mark.parametrize('tag_value', [item.value for item in Compression])
+def test_dataset_compression(path_rgb_byte_tif, tag_value):
+    """Compression is found from tags"""
+    with rasterio.open(path_rgb_byte_tif) as dataset:
+        dataset.tags = MagicMock()
+        dataset.tags.return_value = {'COMPRESSION': tag_value}
+        assert dataset.compression == Compression(tag_value)
