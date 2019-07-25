598d88f29ccaa9dfa72840a4ddac78471ef995de
Sean Gillies <sean@mapbox.com>
2019-07-25 17:16:13 -0600

    Test for share/proj

--- scripts/travis_proj_install.sh
+++ scripts/travis_proj_install.sh
@@ -14,13 +14,15 @@ ls -l $PROJINST
 
 echo "PROJ VERSION: $PROJVERSION"
 
-cd $PROJBUILD
-wget -q https://download.osgeo.org/proj/proj-$PROJVERSION.tar.gz
-tar -xzf proj-$PROJVERSION.tar.gz
-cd proj-$PROJVERSION
-./configure --prefix=$PROJINST/gdal-$GDALVERSION
-make -s -j 2
-make install
+if [ ! -d "$PROJINST/gdal-$GDALVERSION/share/proj" ]; then
+    cd $PROJBUILD
+    wget -q https://download.osgeo.org/proj/proj-$PROJVERSION.tar.gz
+    tar -xzf proj-$PROJVERSION.tar.gz
+    cd proj-$PROJVERSION
+    ./configure --prefix=$PROJINST/gdal-$GDALVERSION
+    make -s -j 2
+    make install
+fi
 
 # change back to travis build dir
 cd $TRAVIS_BUILD_DIR
