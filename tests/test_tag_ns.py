d819275a9b1a530e76dc204454afe1bb47a9cac1
Sean Gillies <sean@mapbox.com>
2019-10-01 12:39:51 -0600

    Split tests into two functions, note changes

--- tests/test_tag_ns.py
+++ tests/test_tag_ns.py
@@ -1,16 +1,15 @@
-#-*- coding: utf-8 -*-
-import logging
-import sys
+# -*- coding: utf-8 -*-
 
 import rasterio
 
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+def test_get_tag_item():
+    """Should return the correct list of dataset tag namespaces."""
+    with rasterio.open('tests/data/cogeo.tif') as src:
+        assert src.tag_namespaces() == ['IMAGE_STRUCTURE', 'DERIVED_SUBDATASETS']
 
 
 def test_get_tag_item():
-    """Should return the correct list of tag namespaces."""
+    """Should return the correct list of band tag namespaces."""
     with rasterio.open('tests/data/cogeo.tif') as src:
-        assert src.get_tag_ns() == ['IMAGE_STRUCTURE', 'DERIVED_SUBDATASETS']
-
-    with rasterio.open('tests/data/cogeo.tif') as src:
-        assert src.get_tag_ns(bidx=1) == []
+        assert src.tag_namespaces(bidx=1) == []
