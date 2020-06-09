1bcd5c906676d671fe1f2472a164692aae57c663
Guillaume Lostis <g.lostis@kayrros.com>
2020-06-09 16:22:33 +0200

    Add xfail test for gauss resampling in rio merge

--- tests/test_rio_merge.py
+++ tests/test_rio_merge.py
@@ -566,7 +566,11 @@ def test_data_dir_resampling(tmpdir):
     return tmpdir
 
 
-@pytest.mark.parametrize("resampling", [resamp for resamp in Resampling if resamp < 7])
+@pytest.mark.parametrize(
+    "resampling",
+    [resamp for resamp in Resampling if resamp < 7] +
+    [pytest.param(Resampling.gauss, marks=pytest.mark.xfail)]
+)
 def test_merge_resampling(test_data_dir_resampling, resampling, runner):
     outputname = str(test_data_dir_resampling.join('merged.tif'))
     inputs = [str(x) for x in test_data_dir_resampling.listdir()]
