5433425cd84b193b2f38bcfd6817c6e30aef1d91
Sean Gillies <sean@mapbox.com>
2020-06-11 13:48:41 -0600

    Update change log, change version to 1.1.6dev

--- rasterio/__init__.py
+++ rasterio/__init__.py
@@ -41,7 +41,7 @@ import rasterio.enums
 import rasterio.path
 
 __all__ = ['band', 'open', 'pad', 'Env']
-__version__ = "1.1.5"
+__version__ = "1.1.6dev"
 __gdal_version__ = gdal_version()
 
 # Rasterio attaches NullHandler to the 'rasterio' logger and its
