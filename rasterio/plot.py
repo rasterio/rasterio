3006c8eeeaef73ad51c9dee9f0356d62562a85d7
Louis Burtz <ljburtz@gmail.com>
2020-06-11 15:11:07 +0900

    add docstring

--- rasterio/plot.py
+++ rasterio/plot.py
@@ -240,6 +240,9 @@ def show_hist(source, bins=10, masked=True, title='Histogram', ax=None, label=No
         Title for the figure.
     ax : matplotlib axes (opt)
         The raster will be added to this axes if passed.
+    label : matplotlib labels (opt)
+        If passed, matplotlib will use this label list.
+        Otherwise, a default label list will be automatically created
     **kwargs : optional keyword arguments
         These will be passed to the matplotlib hist method. See full list at:
         http://matplotlib.org/api/axes_api.html?highlight=imshow#matplotlib.axes.Axes.hist
