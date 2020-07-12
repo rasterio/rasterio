fac30a01cddd485b99ed06b8aedc9534d73572d8
snowman2 <alansnow21@gmail.com>
2020-07-11 19:37:11 -0500

    CLN: InMemoryRaster initialize values with cinit & other cython fixes

--- rasterio/_fill.pyx
+++ rasterio/_fill.pyx
@@ -12,23 +12,27 @@ def _fillnodata(image, mask, double max_search_distance=100.0,
     cdef GDALRasterBandH image_band = NULL
     cdef GDALRasterBandH mask_band = NULL
     cdef char **alg_options = NULL
-
-    # copy numpy ndarray into an in-memory dataset.
-    image_dataset = InMemoryRaster(image)
-    image_band = image_dataset.band(1)
-
-    if mask is not None:
-        mask_cast = mask.astype('uint8')
-        mask_dataset = InMemoryRaster(mask_cast)
-        mask_band = mask_dataset.band(1)
+    cdef InMemoryRaster image_dataset = None
+    cdef InMemoryRaster mask_dataset = None
 
     try:
+        # copy numpy ndarray into an in-memory dataset.
+        image_dataset = InMemoryRaster(image)
+        image_band = image_dataset.band(1)
+
+        if mask is not None:
+            mask_cast = mask.astype('uint8')
+            mask_dataset = InMemoryRaster(mask_cast)
+            mask_band = mask_dataset.band(1)
+
         alg_options = CSLSetNameValue(alg_options, "TEMP_FILE_DRIVER", "MEM")
         exc_wrap_int(
             GDALFillNodata(image_band, mask_band, max_search_distance, 0,
                            smoothing_iterations, alg_options, NULL, NULL))
         return image_dataset.read()
     finally:
-        image_dataset.close()
-        mask_dataset.close()
+        if image_dataset is not None:
+            image_dataset.close()
+        if mask_dataset is not None:
+            mask_dataset.close()
         CSLDestroy(alg_options)
