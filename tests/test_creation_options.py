d9c1d602b77b1325caad59acb78079968ee75ce0
Sean Gillies <sean@mapbox.com>
2020-09-11 11:04:39 -0600

    Try to make test more compatible with older pytest

--- tests/test_creation_options.py
+++ tests/test_creation_options.py
@@ -11,18 +11,15 @@ def test_warning(tmpdir, caplog):
     profile = DefaultGTiffProfile(
         count=1, height=256, width=256, compression="lolwut", foo="bar"
     )
-    with caplog.at_level(logging.WARNING, logger="rasterio"):
-        with rasterio.Env(GDAL_VALIDATE_CREATION_OPTIONS=True):
-            rasterio.open(str(tmpdir.join("test.tif")), "w", **profile)
-    assert caplog.record_tuples == [
-        (
-            "rasterio._env",
-            logging.WARNING,
-            "CPLE_NotSupported in driver GTiff does not support creation option COMPRESSION",
-        ),
-        (
-            "rasterio._env",
-            logging.WARNING,
-            "CPLE_NotSupported in driver GTiff does not support creation option FOO",
-        ),
+
+    with rasterio.Env(GDAL_VALIDATE_CREATION_OPTIONS=True):
+        rasterio.open(str(tmpdir.join("test.tif")), "w", **profile)
+
+    assert [
+        "CPLE_NotSupported in driver GTiff does not support creation option COMPRESSION",
+        "CPLE_NotSupported in driver GTiff does not support creation option FOO",
+    ] == [
+        rec.message
+        for rec in caplog.records
+        if rec.levelno == logging.WARNING and rec.name == "rasterio._env"
     ]
