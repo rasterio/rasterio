4d0e38b2f4610a980fba863fe7941d23e6c44881
Ryan Grout <ryan@ryangrout.org>
2021-04-28 13:19:30 -0500

    Rework intersection of bounding boxes.

--- rasterio/coords.py
+++ rasterio/coords.py
@@ -38,16 +38,49 @@ def disjoint_bounds(bounds1, bounds2):
     ``True`` if bounds are disjoint,
     ``False`` if bounds overlap
     """
-    bounds1_north_up = bounds1[3] > bounds1[1]
-    bounds2_north_up = bounds2[3] > bounds2[1]
+    try:
+        intersect_bounds(bounds1, bounds2)
+        return True
+    except BoundsError:
+        return False
 
-    if not bounds1_north_up and bounds2_north_up:
-        # or both south-up (also True)
-        raise ValueError("Bounds must both have the same orientation")
 
-    if bounds1_north_up:
-        return (bounds1[0] > bounds2[2] or bounds2[0] > bounds1[2] or
-                bounds1[1] > bounds2[3] or bounds2[1] > bounds1[3])
-    else:
-        return (bounds1[0] > bounds2[2] or bounds2[0] > bounds1[2] or
-                bounds1[3] > bounds2[1] or bounds2[3] > bounds1[1])
+def reorient_bound(bound):
+    def order(a, b):
+        if b < a:
+            return b, a
+        return a, b
+
+    l, b, r, t = bound
+    if l < r and b < t:
+        return BoundingBox(*bound)
+    l, r = order(l, r)
+    b, t = order(b, t)
+    return BoundingBox(l, b, r, t)
+
+
+def intersect_bounds(bounds1, bounds2):
+    """Return the intersection of two bounds if it exists
+
+    Parameters
+    ----------
+    bounds1: 4-tuple
+        rasterio bounds tuple (left, bottom, right, top)
+    bounds2: 4-tuple
+        rasterio bounds tuple
+
+    Returns
+    -------
+    BoundingBox
+    """
+    bounds1 = reorient_bound(bounds1)
+    bounds2 = reorient_bound(bounds2)
+
+    int_w = max(bounds1[0], bounds2[0])
+    int_e = min(bounds1[2], bounds2[2])
+    int_s = max(bounds1[1], bounds2[1])
+    int_n = min(bounds1[3], bounds2[3])
+
+    if int_w < int_e and int_s < int_n:
+        return BoundingBox(int_w, int_s, int_e, int_n)
+    raise BoundsError("empty intersection")
