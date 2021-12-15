822bf4750e3d89490d8ed8a9c14f3337c581b57b
Vincent Sarago <vincent.sarago@gmail.com>
2021-12-15 22:07:10 +0100

    use local GOES16 file and ignore tests failure for previous GDAL version (#2367)
    * use local Goes16 file for tests
    
    * change err level for test
    
    * ignore fail for gdal <3.3
    
    * compare version

--- tests/test_err.py
+++ tests/test_err.py
@@ -1,10 +1,14 @@
 """Testing related to GDAL CPLError handling."""
 
+import logging
 import pytest
 
 import rasterio
 from rasterio._err import CPLE_BaseError
 from rasterio.errors import RasterioIOError
+from rasterio.env import GDALVersion
+
+from .conftest import gdal_version
 
 
 def test_io_error(tmpdir):
@@ -31,31 +35,36 @@ def test_cplerror_str():
     assert str(err) == "test123"
 
 
+@pytest.mark.xfail(gdal_version < GDALVersion(3, 3), reason="GDAL <3.3 will not warn")
 def test_issue2353(caplog, path_rgb_byte_tif):
     """Ensure transformer doesn't leave errors behind."""
     from rasterio.warp import calculate_default_transform
 
-    with rasterio.open(path_rgb_byte_tif) as src:
-        _ = src.colorinterp
-        t, w, h = calculate_default_transform(
-            'PROJCS["unknown",GEOGCS["unknown",DATUM["unknown",SPHEROID["GRS 1980",6378137,298.257222096042]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433]],PROJECTION["Geostationary_Satellite"],PARAMETER["central_meridian",-137],PARAMETER["satellite_height",35786023],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],EXTENSION["PROJ4","+proj=geos +sweep=x +lon_0=-137 +h=35786023 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"]]',
-            "EPSG:4326",
-            21696,
-            21696,
-            -5434894.885056,
-            -5434894.885056,
-            5434894.885056,
-            5434894.885056,
-        )
-        assert "Ignoring error" in caplog.text
-        _ = src.colorinterp
+    with caplog.at_level(logging.INFO):
+        with rasterio.open(path_rgb_byte_tif) as src:
+            _ = src.colorinterp
+            t, w, h = calculate_default_transform(
+                'PROJCS["unknown",GEOGCS["unknown",DATUM["unknown",SPHEROID["GRS 1980",6378137,298.257222096042]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433]],PROJECTION["Geostationary_Satellite"],PARAMETER["central_meridian",-137],PARAMETER["satellite_height",35786023],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],EXTENSION["PROJ4","+proj=geos +sweep=x +lon_0=-137 +h=35786023 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"]]',
+                "EPSG:4326",
+                21696,
+                21696,
+                -5434894.885056,
+                -5434894.885056,
+                5434894.885056,
+                5434894.885056,
+            )
+            assert "Ignoring error" in caplog.text
+            _ = src.colorinterp
 
 
+@pytest.mark.xfail(gdal_version < GDALVersion(3, 3), reason="GDAL <3.3 will not warn")
 def test_issue2353bis(caplog, path_rgb_byte_tif):
     """Ensure VRT doesn't leave errors behind."""
     from rasterio.vrt import WarpedVRT
-    with rasterio.open("http://static2.skysight.io/demo2.tiff") as src:
-        with WarpedVRT(src, dst_crs="EPSG:3857") as vrt:
-            pass
-        assert "Ignoring error" in caplog.text
-        _ = src.colorinterp
+
+    with caplog.at_level(logging.INFO):
+        with rasterio.open('tests/data/goes.tif') as src:
+            with WarpedVRT(src, dst_crs="EPSG:3857") as vrt:
+                pass
+            assert "Ignoring error" in caplog.text
+            _ = src.colorinterp
