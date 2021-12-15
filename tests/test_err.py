b375100089a56d05f64916d63ed0d5d066413086
vincentsarago <vincent.sarago@gmail.com>
2021-12-15 09:20:49 +0100

    change err level for test

--- tests/test_err.py
+++ tests/test_err.py
@@ -1,5 +1,6 @@
 """Testing related to GDAL CPLError handling."""
 
+import logging
 import pytest
 
 import rasterio
@@ -35,27 +36,30 @@ def test_issue2353(caplog, path_rgb_byte_tif):
     """Ensure transformer doesn't leave errors behind."""
     from rasterio.warp import calculate_default_transform
 
-    with rasterio.open(path_rgb_byte_tif) as src:
-        _ = src.colorinterp
-        t, w, h = calculate_default_transform(
-            'PROJCS["unknown",GEOGCS["unknown",DATUM["unknown",SPHEROID["GRS 1980",6378137,298.257222096042]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433]],PROJECTION["Geostationary_Satellite"],PARAMETER["central_meridian",-137],PARAMETER["satellite_height",35786023],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],EXTENSION["PROJ4","+proj=geos +sweep=x +lon_0=-137 +h=35786023 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"]]',
-            "EPSG:4326",
-            21696,
-            21696,
-            -5434894.885056,
-            -5434894.885056,
-            5434894.885056,
-            5434894.885056,
-        )
-        assert "Ignoring error" in caplog.text
-        _ = src.colorinterp
+    with caplog.at_level(logging.INFO):
+        with rasterio.open(path_rgb_byte_tif) as src:
+            _ = src.colorinterp
+            t, w, h = calculate_default_transform(
+                'PROJCS["unknown",GEOGCS["unknown",DATUM["unknown",SPHEROID["GRS 1980",6378137,298.257222096042]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433]],PROJECTION["Geostationary_Satellite"],PARAMETER["central_meridian",-137],PARAMETER["satellite_height",35786023],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],EXTENSION["PROJ4","+proj=geos +sweep=x +lon_0=-137 +h=35786023 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"]]',
+                "EPSG:4326",
+                21696,
+                21696,
+                -5434894.885056,
+                -5434894.885056,
+                5434894.885056,
+                5434894.885056,
+            )
+            assert "Ignoring error" in caplog.text
+            _ = src.colorinterp
 
 
 def test_issue2353bis(caplog, path_rgb_byte_tif):
     """Ensure VRT doesn't leave errors behind."""
     from rasterio.vrt import WarpedVRT
-    with rasterio.open('tests/data/goes.tif') as src:
-        with WarpedVRT(src, dst_crs="EPSG:3857") as vrt:
-            pass
-        assert "Ignoring error" in caplog.text
-        _ = src.colorinterp
+
+    with caplog.at_level(logging.INFO):
+        with rasterio.open('tests/data/goes.tif') as src:
+            with WarpedVRT(src, dst_crs="EPSG:3857") as vrt:
+                pass
+            assert "Ignoring error" in caplog.text
+            _ = src.colorinterp
