fc12b80285e8d22e3e9bfc4d95797c99eb4a2c27
Marcin Swaltek <swaltek@tutanota.com>
2022-07-28 22:00:42 +0200

    union type for tuple
    Co-authored-by: Alan D. Snow <alansnow21@gmail.com>

--- rasterio/_base.pyx
+++ rasterio/_base.pyx
@@ -680,7 +680,7 @@ cdef class DatasetBase:
 
         Returns
         -------
-        tuple
+        tuple[str | None, ...]
         """
         if not self._descriptions:
             descr = [GDALGetDescription(self.band(j)) for j in self.indexes]
