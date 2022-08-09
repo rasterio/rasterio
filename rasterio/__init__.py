522c8e12e2e68ead60d5949c353d9cb4eeef41b0
Sean Gillies <sean.gillies@gmail.com>
2022-08-09 16:44:31 -0600

    This is 1.3.0.post1

--- rasterio/__init__.py
+++ rasterio/__init__.py
@@ -55,7 +55,7 @@ with rasterio._loading.add_gdal_dll_directories():
         have_vsi_plugin = False
 
 __all__ = ['band', 'open', 'pad', 'Env', 'CRS']
-__version__ = "1.3.0"
+__version__ = "1.3.0.post1"
 __gdal_version__ = gdal_version()
 __proj_version__ = ".".join([str(version) for version in get_proj_version()])
 __geos_version__ = ".".join([str(version) for version in get_geos_version()])
