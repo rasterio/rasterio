5e3938816a8b45dfe381167f38af71877e991c57
Ryan Grout <ryan@ryangrout.org>
2022-08-24 16:45:04 -0500

    Add test.
    Test bounds computation when transform has almost zero values.

--- tests/test_read.py
+++ tests/test_read.py
@@ -3,10 +3,12 @@ import unittest
 
 import numpy as np
 import pytest
+import sys
 
 import rasterio
 from rasterio._err import CPLE_AppDefinedError
 from rasterio.errors import DatasetIOShapeError, RasterioIOError
+import affine
 
 # Find out if we've got HDF support (needed below).
 try:
@@ -15,6 +17,30 @@ try:
     has_hdf = True
 except Exception:
     has_hdf = False
+
+
+# Fixture to create test datasets within temporary directory
+@pytest.fixture(scope='function')
+def test_data_dir(tmpdir):
+    eps = sys.float_info.epsilon
+    kwargs = {
+        "crs": {'init': 'epsg:4326'},
+        "transform": affine.Affine(0.5, eps, 13,
+                                   eps, 1.2, -32),
+        "count": 1,
+        "dtype": rasterio.uint8,
+        "driver": "GTiff",
+        "width": 10,
+        "height": 10,
+        "nodata": 1
+    }
+
+    with rasterio.open(str(tmpdir.join('a.tif')), 'w', **kwargs) as dst:
+        data = np.ones((10, 10), dtype=rasterio.uint8)
+        data[0:6, 0:6] = 255
+        dst.write(data, indexes=1)
+
+    return tmpdir
 
 
 class ReaderContextTest(unittest.TestCase):
@@ -363,3 +389,9 @@ def test_chained_io_errors(path_rgb_byte_tif):
         assert isinstance(exc, CPLE_AppDefinedError)
         msg = str(exc)
         assert "TIFFFillTile:Read error" in msg
+
+
+def test_read_bounds_eps(test_data_dir):
+    breakpoint()
+    with rasterio.open(str(test_data_dir/"a.tif")) as ds:
+        assert ds.bounds == (13.0, -20.0, 18.0, -32.0)
