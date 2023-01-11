37a644ef5c3022b080eb8b9f4262e055733ad022
Sean Gillies <sean.gillies@gmail.com>
2023-01-10 20:32:55 -0700

    Draft of a VirtualDataset class

--- tests/test_vrt.py
+++ tests/test_vrt.py
@@ -1,5 +1,7 @@
 """Tests of the rasterio.vrt module"""
 
+from pathlib import Path
+
 import rasterio
 import rasterio.vrt
 
@@ -7,7 +9,7 @@ import rasterio.vrt
 def test_boundless_vrt(path_rgb_byte_tif):
     with rasterio.open(path_rgb_byte_tif) as rgb:
         doc = rasterio.vrt._boundless_vrt_doc(rgb)
-        assert doc.startswith('<VRTDataset')
+        assert doc.startswith("<VRTDataset")
         with rasterio.open(doc) as vrt:
             assert rgb.count == vrt.count
             assert rgb.dtypes == vrt.dtypes
@@ -17,8 +19,32 @@ def test_boundless_vrt(path_rgb_byte_tif):
 def test_boundless_msk_vrt(path_rgb_msk_byte_tif):
     with rasterio.open(path_rgb_msk_byte_tif) as msk:
         doc = rasterio.vrt._boundless_vrt_doc(msk)
-        assert doc.startswith('<VRTDataset')
+        assert doc.startswith("<VRTDataset")
         with rasterio.open(doc) as vrt:
             assert msk.count == vrt.count
             assert msk.dtypes == vrt.dtypes
             assert msk.mask_flag_enums == vrt.mask_flag_enums
+
+
+def test_virtual_dataset(path_rgb_byte_tif):
+    with rasterio.open(path_rgb_byte_tif) as rgb:
+        doc = rasterio.vrt._boundless_vrt_doc(rgb)
+        with rasterio.vrt.VirtualDataset.fromstring(
+            doc
+        ) as vrtfile, vrtfile.open() as vrt:
+            assert rgb.count == vrt.count
+            assert rgb.dtypes == vrt.dtypes
+            assert rgb.mask_flag_enums == vrt.mask_flag_enums
+
+
+def test_virtual_dataset_invalid():
+    doc = "<VRTDataset></VRTDataset>"
+    with rasterio.Env() as env:
+        with rasterio.vrt.VirtualDataset.fromstring(doc) as vrtfile:
+            print(vrtfile._memfile.name)
+
+        assert vrtfile._memfile.closed
+        assert (
+            Path(*Path(vrtfile._memfile.name).parts[2:]).parent.as_posix()
+            not in env._dump_vsimem()
+        )
