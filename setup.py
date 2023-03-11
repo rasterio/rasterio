9a711f1f0639c4bec1f0877e9bd3e5c86d30d2c7
Alan D. Snow <alansnow21@gmail.com>
2023-03-10 20:50:36 -0600

    DEP: Add Python 3.11 metadata (#2786)

--- setup.py
+++ setup.py
@@ -312,6 +312,7 @@ setup_args = dict(
         "Programming Language :: Cython",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3",
         "Topic :: Multimedia :: Graphics :: Graphics Conversion",
         "Topic :: Scientific/Engineering :: GIS",
