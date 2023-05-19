e6832dbe15919697f8d4bdc44f3e6441464b31f3
Sean Gillies <sean.gillies@gmail.com>
2023-05-19 14:41:40 -0600

    Extend sieve's input to allow opened datasets and multiband objects
    Resolves #2782

--- tests/test_features.py
+++ tests/test_features.py
@@ -1272,3 +1272,31 @@ def test_zz_no_dataset_leaks(capfd):
         env._dump_open_datasets()
         captured = capfd.readouterr()
         assert not captured.err
+
+
+def test_sieve_bands(pixelated_image, pixelated_image_file):
+    """Verify fix for gh-2782."""
+    truth = sieve(pixelated_image, 9)
+
+    with rasterio.open(pixelated_image_file) as src:
+        assert np.array_equal(truth, sieve(rasterio.band(src, [1]), 9))
+
+        # Mask band should also work but will be a no-op
+        assert np.array_equal(
+            pixelated_image,
+            sieve(rasterio.band(src, [1]), 9, mask=rasterio.band(src, 1))
+        )
+
+
+def test_sieve_dataset(pixelated_image, pixelated_image_file):
+    """Verify fix for gh-2782."""
+    truth = sieve(pixelated_image, 9)
+
+    with rasterio.open(pixelated_image_file) as src:
+        assert np.array_equal(truth, sieve(src, 9))
+
+        # Mask band should also work but will be a no-op
+        assert np.array_equal(
+            pixelated_image,
+            sieve(src, 9, mask=rasterio.band(src, 1))
+        )
