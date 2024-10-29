3e1514f4f2339dcf6608fbd0a87c9d868e224d44
Sean Gillies <sean.gillies@gmail.com>
2024-10-29 08:13:18 -0600

    Reduce shape of destination for the 1-D case. (#3223)
    Resolves #3215

--- tests/test_warp.py
+++ tests/test_warp.py
@@ -1362,6 +1362,7 @@ def test_reproject_masked(test3d, count_nonzero, path_rgb_byte_tif):
         dst_nodata=99,
     )
     assert np.ma.is_masked(source)
+    assert source.shape == out.shape
     assert np.count_nonzero(out[out != 99]) == count_nonzero
     assert not np.ma.is_masked(out)
 
@@ -1400,6 +1401,7 @@ def test_reproject_masked_masked_output(test3d, count_nonzero, path_rgb_byte_tif
         dst_transform=DST_TRANSFORM,
         dst_crs="EPSG:3857",
     )
+    assert inp.shape == out.shape
     assert np.count_nonzero(out[out != np.ma.masked]) == count_nonzero
 
 
