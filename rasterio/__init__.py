2d691969dcc394b771c2f156eb960e63287d33ee
snowman2 <alansnow21@gmail.com>
2025-12-05 11:41:44 -0600

    MNT: 1.4.4.dev0

--- rasterio/__init__.py
+++ rasterio/__init__.py
@@ -82,7 +82,7 @@ except ImportError:
     have_vsi_plugin = False
 
 __all__ = ['band', 'open', 'pad', 'Band', 'Env', 'CRS']
-__version__ = "1.4.4dev"
+__version__ = "1.4.4.dev0"
 __gdal_version__ = gdal_version()
 __proj_version__ = ".".join([str(version) for version in get_proj_version()])
 __geos_version__ = ".".join([str(version) for version in get_geos_version()])
