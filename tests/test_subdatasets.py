4964ea9157ac601b042f9ce6944e2a7e708bced3
snowman2 <alansnow21@gmail.com>
2026-02-18 08:43:40 -0600

    BUG: Preserve quotes in subdataset path

--- tests/test_subdatasets.py
+++ tests/test_subdatasets.py
@@ -1,4 +1,5 @@
 import pytest
+import shutil
 
 import rasterio
 
@@ -15,6 +16,20 @@ def test_subdatasets():
         assert len(subs) == 3
         for name in subs:
             assert name.startswith("netcdf")
+
+
+@pytest.mark.skipif(not HAVE_NETCDF, reason="GDAL not compiled with NetCDF driver.")
+def test_subdatasets__parsing_colons(tmp_path):
+    """Ensure paths with ':' can be opened as subdatasets"""
+    test_file = tmp_path / "RGB_2026-01-01T00:00:00.nc"
+    shutil.copy("tests/data/RGB.nc", test_file)
+    with rasterio.open(test_file) as src:
+        subs = src.subdatasets
+        assert len(subs) == 3
+        for path in subs:
+            assert path.startswith("netcdf")
+            with rasterio.open(path):
+                pass
 
 
 @pytest.mark.skipif(not HAVE_HDF5, reason="GDAL not compiled with HDF5 driver.")
