68fdd37cc2abab67eb7f9acb3ba176ef6e3c9813
Sylvester <100048048+w8sl@users.noreply.github.com>
2026-03-10 15:49:44 +0100

    Change HDF5 download source to the official GitHub repository
    Better download link naming consistency across versions

--- ci/config.sh
+++ ci/config.sh
@@ -280,7 +280,7 @@ GEOS_URL="http://download.osgeo.org/geos/geos-${GEOS_VERSION}.tar.bz2"
 GEOS_FNAME="geos-${GEOS_VERSION}"
 fetch_untar ${GEOS_URL} ${GEOS_FNAME}.tar.bz2
 
-HDF5_URL=https://support.hdfgroup.org/releases/hdf5/${HDF5_VERSION}/downloads/hdf5.tar.gz
+HDF5_URL=http://github.com/HDFGroup/hdf5/releases/download/${HDF5_VERSION}/hdf5-${HDF5_VERSION}.tar.gz
 HDF5_FNAME="hdf5-${HDF5_VERSION}"
 HDF5_SHA256="ce7f5515a95d588b8606c3fb50643f8b88ac52ffbbde9c63bb1edca6a256e964"
 fetch_untar ${HDF5_URL} -O ${HDF5_FNAME}.tar.gz ${HDF5_SHA256}
