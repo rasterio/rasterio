b2579409b6c687cf12bc0b6d3fc191ee2c1c8370
Mike Taves <mwtoews@gmail.com>
2026-03-30 14:04:44 +1300

    Limit to sphinx 8.2, remove "defaults" Anaconda channel, exclude other dirs

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,7 @@ language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build"]
+exclude_patterns = ["_build", ".venv", ".pixi"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
